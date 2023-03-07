4eac061bb9b3495d9a4ce68e0cae36893628f007
MaksimMalkovV <maer2971@gmail.com>
2023-03-07 21:47:22 +0300

    created a project

---
+++ portfolio/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+
+def portfolio(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/portfolio.html', {'projects':projects})

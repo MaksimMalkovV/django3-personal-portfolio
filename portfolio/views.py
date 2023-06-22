77f028310e54c1006c4bb8c1698c19047b33c9c9
Maksim Malkov <113548800+MaksimMalkovV@users.noreply.github.com>
2023-06-22 12:59:51 +0300

    Delete portfolio directory

--- portfolio/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Project
-
-def portfolio(request):
-    projects = Project.objects.all()
-    return render(request, 'portfolio/portfolio.html', {'projects':projects})
